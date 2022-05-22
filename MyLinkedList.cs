b7ece1730767f0ec322462e2b57cc5771d08c0bd
江南雨 <154519213@qq.com>
2022-05-22 18:12:24 +0800

    LinkedList

---
+++ MyLinkedList.cs
@@ -0,0 +1,36 @@
+
+public class MyLinkedList
+{
+    private MyLinkedList next;
+    private int val;
+    
+    public void Console()
+    {
+        System.Console.WriteLine("这里是自己实现的链表");
+    }
+    public MyLinkedList() {
+        Console();
+    }
+    
+    public int Get(int index) {
+        return -1;
+    }
+    
+    public void AddAtHead(int val) {
+
+    }
+    
+    public void AddAtTail(int val) {
+
+    }
+    
+    public void AddAtIndex(int index, int val) {
+
+    }
+    
+    public void DeleteAtIndex(int index) {
+
+    }
+
+
+}
